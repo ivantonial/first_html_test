27110e3677c8fd48161a54a6f878eb0fc972b16e
Ivan Tonial <ivantonial@gmail.com>
2021-11-21 06:58:44 -0300

    querySelector

---
+++ Pages/03-javascript/03-queryselector/queryselector.js
@@ -0,0 +1,87 @@
+// Titulo
+document.querySelector(".headerTitle").innerHTML = "About my favorite band!";
+
+// Minha imagem
+let eu = document.querySelector(".ivanTonial");
+eu.src = "./assets/images/IvanRamalhoTonial.jpeg";
+
+
+// Nome - idade - estado
+document.querySelector(".name").innerHTML = "Name: Ivan Ramalho Tonial";
+document.querySelector(".age").innerHTML = "Age: 32 years old";
+document.querySelector(".place").innerHTML = "State: Rio de Janeiro";
+
+// images
+let img01 = document.querySelector(".img01");
+img01.src = "assets/images/01-ThePiperAtTheGatesOfDawn-1967.webp"
+let img02 = document.querySelector(".img02");
+img02.src = "assets/images/02-ASaucerfulOfSecrets1968.webp"
+let img03 = document.querySelector(".img03");
+img03.src = "assets/images/03-More1969.webp"
+let img04 = document.querySelector(".img04");
+img04.src = "assets/images/04-Ummagumma1969.webp"
+let img05 = document.querySelector(".img05");
+img05.src = "assets/images/05-AtomHeartMother1970.webp"
+let img06 = document.querySelector(".img06");
+img06.src = "assets/images/06-Meddle1971.webp"
+let img07 = document.querySelector(".img07");
+img07.src = "assets/images/07-ObscuredByClouds1972.webp"
+let img08 = document.querySelector(".img08");
+img08.src = "assets/images/08-TheDarkSideOfTheMoon1973.webp"
+let img09 = document.querySelector(".img09");
+img09.src = "assets/images/09-WishYouWereHere1975.webp"
+let img10 = document.querySelector(".img10");
+img10.src = "assets/images/10-Animals1977.webp"
+let img11 = document.querySelector(".img11");
+img11.src = "assets/images/11-TheWall1979.webp"
+let img12 = document.querySelector(".img12");
+img12.src = "assets/images/12-TheFinalCut1983.webp"
+let img13 = document.querySelector(".img13");
+img13.src = "assets/images/13-AMomentaryLapseOfReason1987.webp"
+let img14 = document.querySelector(".img14");
+img14.src = "assets/images/14-TheDivisionBell1994.webp"
+let img15 = document.querySelector(".img15");
+img15.src = "assets/images/15-TheEndlessRiver.webp"
+
+
+// main
+document.querySelector(".favoriteBand").innerHTML = "Pink Floyd";
+document.querySelector(".descriptionP").innerHTML = "Description:&nbsp;&nbsp;&nbsp;";
+document.querySelector(".pinkFloydDiscriptionP1").innerHTML = "A história do Pink Floyd é uma das mais conturbadas já vistas no rock. Não é exagero compará-la a uma novela ou um grande dramalhão com direito a tragédia, sucesso, brigas e 'fortes emoções'. Nesse meio tempo eles também gravaram vários discos de sucesso - incluindo 'Dark Side Of The Moon' o terceiro álbum mais vendido de todos os tempos - e podem ser orgulhar de fazer parte daquele seleto grupo de bandas que todo mundo parece conhecer.";
+document.querySelector(".pinkFloydDiscriptionP2").innerHTML = "De maneira simplista, podemos dividir a carreira do Floyd em quatro fases. A primeira engloba o pouco tempo em que eles foram uma das melhores bandas psicodélicas do planeta sob a liderança do genial Syd Barrett.";
+document.querySelector(".pinkFloydDiscriptionP3").innerHTML = "Infelizmente, o músico acabou expulso do grupo que ele mesmo criou, depois que o excessivo consumo de drogas como o LSD acabaram por comprometer severamente a sua saúde mental.";
+document.querySelector(".pinkFloydDiscriptionP4").innerHTML = "Barrett ainda gravaria dois discos solo - que contaram com a ajuda de seus ex-colegas - antes de abandonar de vez a música e tornar-se um homem recluso e enlouquecido até sua morte em 2006. Apesar de escasso, seu legado é enorme, e Barrett é facilmente, uma das figuras mais influentes de todo o rock.";
+document.querySelector(".pinkFloydDiscriptionP5").innerHTML = "A segunda etapa tem início com a chegada do guitarrista e vocalista David Gilmour assumindo o posto de Barrett. Ela vai de 1968 a 1972, época de discos mais experimentais, que não vendem muito mas ajudam a criar uma boa reputação e dar uma base de fãs para o grupo.";
+document.querySelector(".pinkFloydDiscriptionP6").innerHTML = "Esse período é marcado também por uma certa democracia, com o baixista Roger Waters, Gilmour, o tecladista Rick Wright e mesmo o baterista Nick Mason escrevendo as canções.";
+document.querySelector(".pinkFloydDiscriptionP7").innerHTML = "Com o lançamento de 'The Dark Side Of The Moon' em 1973 essa formação do Pink Floyd atinge sua plenitude artística e comercial. Os quatro finalmente encontram o seu som que segue aberto a experimentações, mas é bem mais acessível e - como logo se veria - universal.";
+document.querySelector(".pinkFloydDiscriptionP8").innerHTML = "É também a partir desse ponto que Waters vai assumindo mais e mais o controle. Progressivamente ele passará a compor quase tudo sozinho e a se responsabilizar pelos conceitos dos discos e shows e os demais aspectos que envolvem o Floyd.";
+document.querySelector(".pinkFloydDiscriptionP9").innerHTML = "Os outros integrantes tornam-se cada vez mais coadjuvantes - ainda que as colaborações do baixista com Gilmour não possam ser menosprezadas - vide 'Wish You Were Here' ou 'Comfortably Numb'.";
+document.querySelector(".pinkFloydDiscriptionP10").innerHTML = "Em 1983, época de 'The Final Cut', a banda já estava se arrastando. Waters disse que aquele era o fim e não acreditou quando Gilmour falou que seguiria em frente.";
+document.querySelector(".pinkFloydDiscriptionP11").innerHTML = "Mas foi isso o que se sucedeu. Em 1987, para o desespero do baixista, Gilmour, Mason e Wright - retornando depois de ter sido expulso em 1979 - estavam com um novo disco no mercado e uma turnê que lotava arenas e estádios mundo afora.";
+document.querySelector(".pinkFloydDiscriptionP12").innerHTML = "Uma grande briga foi travada nos tribunais e muitas farpas trocadas pela imprensa, mas Gilmour acabou ganhando o direito do uso do nome da banda - como consolação Waters ganhou o direito sobre a marca 'The Wall'.";
+document.querySelector(".pinkFloydDiscriptionP13").innerHTML = "A versão da banda liderada pelo guitarrista era bem mais relaxada e pop que aquela do baixista. Ainda que alguns fãs e críticos tenham torcido o nariz, passados alguns anos, os méritos dessa fase agora são visíveis.";
+document.querySelector(".pinkFloydDiscriptionP14").innerHTML = "Depois de 'The Division Bell' (1994) e a sua turnê, Gilmour deixou a banda para trás.";
+document.querySelector(".pinkFloydDiscriptionP15").innerHTML = "Em 2005, a formação clássica do Floyd se reuniu para tocar no festival Live 8 e tudo indicava que aquele seria o ponto final da banda";
+document.querySelector(".pinkFloydDiscriptionP16").innerHTML = "No ano seguinte, Gilmour lançou seu terceiro disco solo - o primeiro em 22 anos - e mais anos de silêncio se seguiram. Até que em 2014, um novo álbum do Floyd foi anunciado. Na verdade 'The Endless River' era um disco basicamente instrumental construído a partir das sobras de 'The Division Bell  e feito como forma de se homenagear Rick Wright, morto em 2008. O guitarrista disse que a banda não iria sair em turnê e que aquele era o disco de despedida do grupo.";
+document.querySelector(".pinkFloydDiscriptionP17").innerHTML = "Ao que tudo indica, ele não estava brincando. Este ano ele retomou sua carreira solo - com 'Rattle That Lock', seu quarto disco, e anunciou uma grande turnê. Turnê essa que chega agora ao Brasil e marca a primeira apresentação do músico no país - ao contrario de Roger Waters que esteve aqui em três ocasiões (2002, 2007 e 2012).";
+document.querySelector(".pinkFloydDiscriptionP18").innerHTML = "Para marcar esse momento, o Vagalume publica esse especial onde falamos um pouco sobre cada um dos 15 álbuns lançados pelo Pink Floyd.";
+
+// footer
+let know = document.querySelector(".toKnowMore");
+know.href = "https://www.pinkfloyd.com/home.php";
+know.innerHTML = "Para saber mais, acesse o site oficial clicando aqui!";
+know.target = "_blank";
+let officialSite = document.querySelector(".officialSite");
+officialSite.href = "https://www.pinkfloyd.com/home.php";
+officialSite.target = "_blank";
+
+
+
+let counter =1;
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 15){
+        counter = 1;
+    }
+}, 2000);
