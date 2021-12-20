40f4e17b74176e40a0ac3573d8c8373575ec77ea
Ivan Tonial <ivantonial@gmail.com>
2021-12-20 20:34:10 -0300

    mega-sena sorteio

---
+++ Pages/03-javascript/18-callback/q2/script.js
@@ -0,0 +1,93 @@
+let count;
+const chosenNumbers = [];
+let interval;
+
+
+//impressão
+function callback(number) {
+    document.getElementById('number' + count).innerHTML = number;
+    count++;
+    chosenNumbers.push(number);
+    if (count == 6) {
+        document.getElementById('btnChoosing').style.visibility = "visible";
+        clearInterval(interval);
+    }
+}
+
+//choosing a number
+function choosing() {
+    chosenNumbers.splice(0, chosenNumbers.length);
+    count = 0;
+    document.getElementById('btnChoosing').style.visibility = "hidden";
+    clearOldNumbers();
+    interval = setInterval(verifying, 1000);
+}
+
+function clearOldNumbers() {
+    for (let i = 0; i < 6; i++) {
+        document.getElementById('number' + i).innerHTML = "";
+    }
+}
+
+//verifying a number
+function verifying() {
+    if (count < 6) {
+        let num = Math.floor(Math.random() * 60 + 1);
+        let compare = true;
+        if (count > 0) {
+            while (compare) {
+                compare = false;
+                chosenNumbers.forEach((element) => {
+                    if (element == num) {
+                        num = Math.floor(Math.random() * 60 + 1);
+                        console.log('repetiu: ' + num);
+                        compare = true;
+                    }
+                });
+            }
+        }
+        callback(num);
+        console.log(chosenNumbers);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function drawNumber(callback) {
+    let number = Math.floor(Math.random() * (60) + 1);
+    while (count < 6) {
+        if (chosenNumbers.length != 0) {
+            for (let i = 0; i < chosenNumbers.length; i++) {
+                if (chosenNumbers[i] == number) {
+                    number = Math.floor(Math.random() * (60) + 1);
+                    i = -1;
+                }
+            }
+        }
+        chosenNumbers.push(number);
+        count++;
+    }
+}
+
+// function printNumber(number) {
+
+// }
+
+
