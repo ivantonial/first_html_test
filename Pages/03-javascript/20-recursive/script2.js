b8c2916c1b2a462c8164f6c5c306d072e1181112
Ivan Tonial <ivantonial@gmail.com>
2022-01-13 13:43:55 -0300

    recursive

---
+++ Pages/03-javascript/20-recursive/script2.js
@@ -0,0 +1,25 @@
+const matrix = [[10,15,5,2],[5,59,48,18],[81,168,19,18],[948,198,918,19]]
+const printNumbers = [];
+
+function printMatrix(_matrix, _lin, _col){
+    const lin = _lin;
+    const col = _col;
+    printNumbers.push(matrix[lin][col]);
+
+    console.log(matrix[lin][col]);
+    if (col === 0 && lin ===0){
+        return printNumbers;
+        //return _matrix[lin][col];
+    }
+    //console.log(matrix[lin][col]);
+
+    if (col === 0){
+        return printMatrix(matrix,lin-1,matrix.length - 1);
+    }
+    else {
+        return printMatrix(matrix,lin,col-1);
+    }
+}
+
+printMatrix(matrix, matrix[0].length - 1, matrix.length - 1);
+console.log(printNumbers);
